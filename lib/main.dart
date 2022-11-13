c739950e533848597efe630363a260679ef0ed7b
Yared11000010 <yaredayele67@gmail.com>
2022-11-13 22:11:28 +0100

    add appbar and stateless wedgets in my projects

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:async';
+import 'package:http/http.dart';
 
 void main() {
   runApp(MaterialApp(
-
+    debugShowCheckedModeBanner: false,
+    title: 'Quakes',
+    home: new Quakes(),
   ));
 }
 
+class Quakes extends StatelessWidget {
+  const Quakes({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text('Quakes'),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+
+      ),
+    );
+  }
+}
