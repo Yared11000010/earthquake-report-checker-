7d2b29b869933f6054e1adcc401219c826a18d22
Yared11000010 <yaredayele67@gmail.com>
2022-11-14 23:44:54 +0100

    add convert millseconds to user visible date and time using utl dart package

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'dart:async';
+import 'package:intl/intl.dart';
 import 'package:http/http.dart' as http;
-
 
 Map? _quakes;
 List? _features;
+
 void main() async {
   _quakes = await getQuakes();
 
-  _features=_quakes!['features'];
+  _features = _quakes!['features'];
   // print(_quakes['features'][0]['properties']);
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
@@ -34,9 +35,40 @@ class Quakes extends StatelessWidget {
             itemCount: _features?.length,
             padding: EdgeInsets.all(15.0),
             itemBuilder: (BuildContext context, int position) {
+              if (position.isOdd) return Divider();
+              final index = position ~/ 2; //
+              // we are dividing positon by 2 and returing an integer result
+              var format=new DateFormat.yMMMMd("en_US").add_jm();
+              var date=format.format(new DateTime.fromMicrosecondsSinceEpoch(_features![index]['properties']['time']*1000,
+              isUtc: true));
+
               return new ListTile(
                 title: new Text(
-                    "${_features![position]['properties']['place']}"),
+                  "At: $date",
+                  style: TextStyle(
+                      fontSize: 16.5,
+                      color: Colors.deepOrange,
+                      fontWeight: FontWeight.w500),
+                ),
+                subtitle: new Text(
+                  "${_features![index]['properties']['place']}",
+                  style: TextStyle(
+                    fontSize: 14.5,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.grey,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+                leading: new CircleAvatar(
+                  backgroundColor: Colors.pink,
+                  child: new Text("${_features![index]['properties']['mag']}",
+                  style: new TextStyle(
+                    fontStyle: FontStyle.italic,
+                    fontSize: 16.5,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500
+                  ),),
+                ),
               );
             }),
       ),
